3681d2e26c02b1da9c6320b6f34a1de3b1e8cb0d
Masssana <120189802+Masssana@users.noreply.github.com>
2025-07-19 13:53:32 +0500

    Delete TestV4/ResumesServ directory

--- TestV4/ResumesServ/src/main/java/ru/college/resumesserv/repository/ResumeRepository.java
+++
@@ -1,6 +0,0 @@
-package ru.college.resumesserv.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import ru.college.resumesserv.CRUD.Resume;
-public interface ResumeRepository extends JpaRepository<Resume, Long>, JpaSpecificationExecutor<Resume> {
-}
